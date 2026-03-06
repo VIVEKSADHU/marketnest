b99d543ec65a04b9b73c2246e151188e4acfee14
VIVEKSADHU <viveksadhu369@gmail.com>
2026-03-06 10:40:54 +0530

    VERCEL DEPLOYMENT LINK

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ const allowedOrigins = (
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://marketnest-two.vercel.app"
     ],
     credentials: true
   })
